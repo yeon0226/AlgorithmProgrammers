3e2eb26e1ad6b9c129b1644397a2fc83c76e26e4
yeon0226 <124769292+yeon0226@users.noreply.github.com>
2023-04-16 10:49:01 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+  let arr = [0, 0];
+  let even = 0;
+  let odd = 0;
+    
+    for(let i = 0; i < num_list.length; i++) {
+        if(num_list[i] % 2 === 0) {
+            even++;
+            arr[0] = even;
+        } else {
+            odd++;
+            arr[1] = odd;
+        }
+    }
+   
+    return arr;
+}
