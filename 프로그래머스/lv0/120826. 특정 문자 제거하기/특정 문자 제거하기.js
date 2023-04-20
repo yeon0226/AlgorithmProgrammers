ba2e018d6ab997555fe0d2586f7ae1c78c5143d8
yeon0226 <124769292+yeon0226@users.noreply.github.com>
2023-04-20 09:34:19 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,12 +1,16 @@
 function solution(my_string, letter) {
-    let result="";
-   for(let i = 0; i < my_string.length; i++){
-       if(my_string[i] !== letter){
-           result = result + my_string[i];
-       }
-   }
-    return result;
+    const answer = my_string.split(letter).join('')
+    return answer;
 }
+// function solution(my_string, letter) {
+//     let result="";
+//    for(let i = 0; i < my_string.length; i++){
+//        if(my_string[i] !== letter){
+//            result = result + my_string[i];
+//        }
+//    }
+//     return result;
+// }
 
 // 문자열에 letter와 같은 문자열이 잇는지 확인
 // 있으면 제거한 문자열 return 
