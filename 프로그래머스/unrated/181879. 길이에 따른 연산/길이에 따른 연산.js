48df66174fde4bd0c1084521c77e2cb40b3e5128
yeon0226 <124769292+yeon0226@users.noreply.github.com>
2023-08-22 10:52:18 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,13 +1,17 @@
+// function solution(num_list) {
+//     let sum = 0;
+//     let result = 1;
+//     if(num_list.length >= 11){
+//         for(let i = 0; i < num_list.length; i++){
+//             sum += num_list[i];
+//         } return sum;
+//     } else{
+//         for(let i = 0; i < num_list.length; i++){
+//             result *= num_list[i];
+//         }return result;
+//     }
+// }
+
 function solution(num_list) {
-    let sum = 0;
-    let result = 1;
-    if(num_list.length >= 11){
-        for(let i = 0; i < num_list.length; i++){
-            sum += num_list[i];
-        } return sum;
-    } else{
-        for(let i = 0; i < num_list.length; i++){
-            result *= num_list[i];
-        }return result;
-    }
+    return num_list.length <= 10 ? num_list.reduce((acc, cur) => acc * cur) : num_list.reduce((acc, cur) => acc + cur)
 }
