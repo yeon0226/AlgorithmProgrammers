329676293f39d3e34154fb5152b5398f2680dc29
yeon0226 <124769292+yeon0226@users.noreply.github.com>
2023-08-23 14:54:22 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,19 +1,26 @@
+// function solution(num_list) {
+    
+//     let total;
+//     let result1=[];
+//     let result2=[];
+    
+//     for(let i=0; i < num_list.length; i++){
+//         if(num_list[i] % 2 !== 0){
+//       result1.push(num_list[i])
+//         } else{
+//       result2.push(num_list[i]);
+//         }
+//     }
+//     // 숫자로 변환
+//     let arr1 = parseInt(result1.join(''));
+//     let arr2 = parseInt(result2.join(''));
+    
+//     return arr1 + arr2;
+// }
+
 function solution(num_list) {
+    let arr1 = num_list.filter(num => num % 2 !== 0).join('');
+    let arr2 = num_list.filter(num => num % 2 === 0).join('');
     
-    let total;
-    let result1=[];
-    let result2=[];
-    
-    for(let i=0; i < num_list.length; i++){
-        if(num_list[i] % 2 !== 0){
-      result1.push(num_list[i])
-        } else{
-      result2.push(num_list[i]);
-        }
-    }
-    // 숫자로 변환
-    let arr1 = parseInt(result1.join(''));
-    let arr2 = parseInt(result2.join(''));
-    
-    return arr1 + arr2;
+    return parseInt(arr1) + parseInt(arr2);
 }
