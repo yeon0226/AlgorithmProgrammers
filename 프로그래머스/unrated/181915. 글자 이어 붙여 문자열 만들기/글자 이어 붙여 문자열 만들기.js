c833e28c92cc9518f80f7d73314419bc9390b45f
yeon0226 <124769292+yeon0226@users.noreply.github.com>
2023-08-29 19:47:43 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_string, index_list) {
+   
+    let result ='';
+    for(let i = 0; i < index_list.length; i++){
+        result += my_string[index_list[i]];
+    }
+    return result
+}
