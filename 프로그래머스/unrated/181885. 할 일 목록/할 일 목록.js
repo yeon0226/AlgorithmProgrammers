707a7c36cfadd306132c55533ab58b7b0486961d
yeon0226 <124769292+yeon0226@users.noreply.github.com>
2023-08-31 10:18:31 +0900

    [unrated] Title: 할 일 목록, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,10 @@
+function solution(todo_list, finished) {
+    let result =[];
+    
+    for(let i = 0; i < todo_list.length; i++){
+        if(!finished[i]){
+            result.push(todo_list[i])
+        }
+    }
+    return result;
+}
